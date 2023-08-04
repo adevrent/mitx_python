2c550adcbbcc1b6227eec4b73e95cfc7cc5d9280
adevrent <a.devrent@hotmail.com>
2023-08-04 14:29:49 +0300

    lander finished

--- CSE.0002x/Problem Set 1/lander.py
+++ CSE.0002x/Problem Set 1/lander.py
@@ -28,7 +28,49 @@ class LanderIVP(IVP):
             f (float list): current forcing
         """
         #### BEGIN SOLUTION ####
-        param = self.atmosphere(z)
+        
+        # Unpack u to V, z
+        V = u[0]
+        z = u[1]
+        #print("u[1] =", u[1])#debug
+        
+        # load parameter values
+        
+        # assign atmospheric parameters
+        #print("z =", z)#debug
+        param = self.atmosphere(z)  # dict
+        Ta = param["Ta"]
+        pa = param["pa"]
+        rhoa = param["rhoa"]
+        g = param["g"]
+
+        m_l = self.get_p("m_l")  # kg
+        A_l = self.get_p("A_l")  # m^2
+        A_p = self.get_p("A_p")  # m^2
+        CD_l = self.get_p("CD_l")  # constant
+        CD_p = self.get_p("CD_p")  # constant
+        theta_e = math.radians(self.get_p("theta_e"))  # rad
+        theta_p = math.radians(self.get_p("theta_p"))  # rad
+        
+        D_l = 0.5 * rhoa * V**2 * A_l * CD_l
+        D_p = 0.5 * rhoa * V**2 * A_p * CD_p
+        
+        # params depending on if parachute is open or not.
+        V_p = self.get_p("V_p")  # (m/s) Parachute is deployed if current velocity <= V_p.
+        if V >= V_p:  # parachute is not deployed.
+            theta = theta_e
+            D = D_l
+        else:  # parachute is deployed.
+            theta = theta_p
+            D = D_l + D_p
+        
+        # calculate f(u, t) where u = (V, z)
+        f = []
+        dV_dt = g * math.cos(theta) - D/m_l  # f[0] is dV/dt
+        dz_dt = -V * math.cos(theta)  # f[1] is dz_dt
+        f.append(dV_dt)
+        f.append(dz_dt)
+        return f
         #### END SOLUTION ####
 
     def atmosphere(self, z):
@@ -46,6 +88,7 @@ class LanderIVP(IVP):
         Returns:
             props (dictionary): property keys and values
         """
+        #print("Type of z:", type(z))#debug
         if z > 7000:
             Ta = 235 - 0.00108*(z-7000) # K
         else:
@@ -90,9 +133,20 @@ def lander_Vzaplot(t, V, z, a, method='numerical'):
         axs (array of Axes): handle to the Axes objects that comprise the figure's subplots
     """
     #### BEGIN SOLUTION ####
-    raise NotImplementedError("Implement lander_Vzaplot")
+    fig, axs = plt.subplots(3, 1, sharex=True)
+    axs[0].plot(t, V, color = "b", label = str(method))
+    axs[0].set_ylabel("$V$ (m/s)")
+    axs[0].legend(loc = "upper right")
+    axs[1].plot(t, z, color = "b")
+    axs[1].set_ylabel("$z$ (km)")
+    axs[2].plot(t, a, color = "b")
+    axs[2].set_ylabel("$a$ (m/s^2)")
+    axs[2].set_xlabel("$t$ (sec)")
+    
+    return axs
+    
+    
     #### END SOLUTION ####
-
 
 def lander_run_case(lander_IVP, dt, mlist):
     """
@@ -114,7 +168,46 @@ def lander_run_case(lander_IVP, dt, mlist):
             * the altitude (z) at which deployment occurred
     """
     #### BEGIN SOLUTION ####
-    raise NotImplementedError("Implement lander_run_case")
+    results = {}
+    
+    for method in mlist:
+        parachute_deployed = False
+        t, u = IVPlib.solve(lander_IVP, dt, method)
+        V = []
+        z = []
+        a = []
+        for i in range(len(t)):
+            V.append(u[i][0])
+            z.append(u[i][1])
+            fa = lander_IVP.evalf(u[i], t[i])
+            a.append(fa[0])  # f[0] is dV/dT = a
+            
+            deployment_condition = (u[i][0] <= lander_IVP.get_p("V_p")) and (parachute_deployed == False)
+            if deployment_condition:
+                parachute_deployed = True
+                #print(u[i][0], "<=", lander_IVP.get_p("V_p"))#debug
+                #print(parachute_deployed)#debug
+                deployment_time = t[i]
+                deployment_z = u[i][1]
+        
+        mname = method.__name__
+        z_km = []
+        for altitude in z:
+            z_km.append(altitude/1000)  # convert to km
+        axs_object = lander_Vzaplot(t, V, z_km, a, method = mname)
+        deployment_z_km = deployment_z/1000
+        results[mname] = (axs_object, deployment_time, deployment_z_km)
+        final_z = z_km[len(z_km)-1]
+        final_V = V[len(V)-1]
+        
+        print("Method:", mname)
+        print("------------------------------------------")
+        print("Parachute deployed at t, z:", "{0:.2e}".format(deployment_time), "s,", "{0:.2e}".format(deployment_z_km), "km")
+        print("Final z, V:", "{0:.2e}".format(final_z), "km,", "{0:.2e}".format(final_V), "m/s")
+        
+    return results
+            
+    
     #### END SOLUTION ####
 
 
