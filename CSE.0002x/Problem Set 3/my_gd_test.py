4ac0992fcc228458aa2ba83df9e9dad0702d073b
adevrent <a.devrent@hotmail.com>
2023-09-07 15:54:12 +0300

    minor fixes

--- CSE.0002x/Problem Set 3/my_gd_test.py
+++ CSE.0002x/Problem Set 3/my_gd_test.py
@@ -3,13 +3,12 @@ import gd_tests
 import numpy as np
 
 
-""" xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J0, xstart = np.array([0.49986708]), alpha = 0.1, nStop = 40, pdict = None)
+xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J0, xstart = np.array([0.49986708]), alpha = 0.1, nStop = 40, pdict = None)
 print(xhist)
-print("xhist shape =", xhist.shape) """
 
-xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J0, xstart = np.array([0.49993354]), alpha = 0.1, nStop = 40, pdict = None)
+""" xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J0, xstart = np.array([0.49993354]), alpha = 0.1, nStop = 40, pdict = None)
 print(xhist)
-print("xhist[0,] =", xhist[0,])
+print("xhist[0,] =", xhist[0,]) """
 
 """ xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J1, xstart = np.array([ 0.41725561, -0.20862781]), alpha = 0.1, nStop = 10, pdict = {'user': np.array([ 0.5 , -0.25])})
 print(xhist)
