d14d19ccafcf57e5754e7abccc25240a11edae7d
adevrent <a.devrent@hotmail.com>
2023-09-07 15:30:19 +0300

    minor fixes

--- CSE.0002x/Problem Set 3/my_gd_test.py
+++ CSE.0002x/Problem Set 3/my_gd_test.py
@@ -5,11 +5,11 @@ import numpy as np
 
 """ xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J0, xstart = np.array([0.49986708]), alpha = 0.1, nStop = 40, pdict = None)
 print(xhist)
-print("xhist[0,] =", xhist[0,]) """
+print("xhist shape =", xhist.shape) """
 
-""" xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J0, xstart = np.array([0.49993354]), alpha = 0.1, nStop = 40, pdict = None)
+xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J0, xstart = np.array([0.49993354]), alpha = 0.1, nStop = 40, pdict = None)
 print(xhist)
-print("xhist[0,] =", xhist[0,]) """
+print("xhist[0,] =", xhist[0,])
 
 """ xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J1, xstart = np.array([ 0.41725561, -0.20862781]), alpha = 0.1, nStop = 10, pdict = {'user': np.array([ 0.5 , -0.25])})
 print(xhist)
@@ -17,8 +17,12 @@ print("xhist[0, 0] =", xhist[0, 0])
 print("xhist[0, 1] =", xhist[0, 1])
 print("xhist[1, 0] =", xhist[1, 0]) """
 
-xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J1, xstart = np.array([ 0.41725561, -0.20862781]), alpha = 0.1, nStop = 10, pdict = {'user': np.array([ 0.5 , -0.25])})
+""" xhist, Jhist = gd.gradient_descent(Jfunc = gd_tests.J1, xstart = np.array([ 0.41725561, -0.20862781]), alpha = 0.1, nStop = 10, pdict = {'user': np.array([ 0.5 , -0.25])})
 print(xhist)
 print("xhist[0, 0] =", xhist[0, 0])
 print("xhist[0, 1] =", xhist[0, 1])
 print("xhist[1, 0] =", xhist[1, 0])
+print("xhist[1, 1] =", xhist[1, 1])
+print("xhist[2, 0] =", xhist[2, 0])
+print("xhist[2, 1] =", xhist[2, 1])
+print("xhist shape =", xhist.shape) """
